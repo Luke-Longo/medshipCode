f807a4f877263d9e3a057fe15b44011fe83b67fd
Luke-Longo <lukelongo0421@gmail.com>
2022-08-16 09:39:40 -0400

    added radio template

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,20 +1,51 @@
 <template>
 	<div>
 		<h3 class="header">Create a new Profile</h3>
-		<div class="flex gap-4">
-			<div class="flex gap-6">
-				<label for="">Practice</label>
-				<input class="" type="radio" v-model="profileType" value="practice" />
+		<div class="flex flex-wrap">
+			<div class="flex items-center mr-4">
+				<input
+					id="red-radio"
+					type="radio"
+					value=""
+					name="colored-radio"
+					class="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				/>
+				<label
+					for="red-radio"
+					class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Red</label
+				>
 			</div>
-			<div class="flex gap-6">
-				<label for="">Sales Rep</label>
-				<input class="" type="radio" v-model="profileType" value="salesRep" />
+			<div class="flex items-center mr-4">
+				<input
+					id="green-radio"
+					type="radio"
+					value=""
+					name="colored-radio"
+					class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				/>
+				<label
+					for="green-radio"
+					class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Green</label
+				>
 			</div>
-			<div class="flex gap-6">
-				<label for="">Admin</label>
-				<input class="" type="radio" v-model="profileType" value="admin" />
+			<div class="flex items-center mr-4">
+				<input
+					id="purple-radio"
+					type="radio"
+					value=""
+					name="colored-radio"
+					class="w-4 h-4 text-purple-600 bg-gray-100 border-gray-300 focus:ring-purple-500 dark:focus:ring-purple-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				/>
+				<label
+					for="purple-radio"
+					class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Purple</label
+				>
 			</div>
 		</div>
+
 		<UiCard class="mx-auto"> </UiCard>
 	</div>
 </template>

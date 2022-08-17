37d32f1f30d48a53696b9ad4ac42bb3db31c1234
Luke-Longo <lukelongo0421@gmail.com>
2022-08-17 10:41:03 -0400

    i AM the GOAT

---
+++ components/Ui/Radio.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="flex justify-center gap-6">
+		<label
+			v-for="radioType in radioTypes"
+			:key="radioType.id"
+			class="cursor-pointer dark:hover:bg-darkBg hover:bg-slate-100 rounded-lg p-2 trans"
+			:class="{ selected: radioType.id === modelValue }"
+			@click="handleUpdate(radioType.id)"
+			>{{ radioType.label }}
+		</label>
+	</div>
+</template>
+
+<script setup lang="ts">
+interface RadioType {
+	id: string;
+	label: string;
+}
+interface Props {
+	radioTypes: RadioType[];
+	modelValue: string;
+}
+const emit = defineEmits<{
+	(e: "update:modelValue", value: string): void;
+}>();
+const props = defineProps<Props>();
+
+const handleUpdate = (type: string) => {
+	emit("update:modelValue", type);
+};
+</script>
+
+<style scoped>
+.selected {
+	@apply dark:bg-darkBg bg-secondary text-white;
+}
+</style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,44 +1,26 @@
 <template>
 	<div>
 		<h3 class="header">Create a new Profile</h3>
-		<div class="flex justify-center gap-6">
-			<label
-				for=""
-				class="cursor-pointer dark:hover:bg-darkBg hover:bg-slate-100 rounded-lg p-2 trans"
-				:class="{ selected: profileType === 'practice' }"
-				@click="handleRadio('practice')"
-				>Practice
-			</label>
-			<label
-				for=""
-				class="cursor-pointer dark:hover:bg-darkBg hover:bg-slate-100 rounded-lg p-2 trans"
-				:class="{ selected: profileType === 'salesRep' }"
-				@click="handleRadio('salesRep')"
-				>Sales Rep
-			</label>
-			<label
-				for=""
-				class="cursor-pointer dark:hover:bg-darkBg hover:bg-slate-100 rounded-lg p-2 trans"
-				:class="{ selected: profileType === 'admin' }"
-				@click="handleRadio('admin')"
-				>Admin
-			</label>
-		</div>
+		<UiRadio :radioTypes="radioTypes" v-model="selectedRadio" />
 	</div>
 </template>
 
 <script setup lang="ts">
-type ProfileType = "practice" | "salesRep" | "admin";
-
-const profileType = ref<ProfileType>("practice");
-
-const handleRadio = (type: ProfileType) => {
-	profileType.value = type;
-};
+const radioTypes = [
+	{
+		id: "practice",
+		label: "Practice",
+	},
+	{
+		id: "salesRep",
+		label: "Sales Rep",
+	},
+	{
+		id: "admin",
+		label: "Admin",
+	},
+];
+const selectedRadio = ref(radioTypes[0].id);
 </script>
 
-<style scoped>
-.selected {
-	@apply dark:bg-darkBg bg-secondary text-white;
-}
-</style>
+<style scoped></style>

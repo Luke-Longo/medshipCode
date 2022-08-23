6e3acb18f1df6151e28e39051139faee4291dee0
Luke-Longo <lukelongo0421@gmail.com>
2022-08-23 15:06:43 -0400

    I need at least one a day

---
+++ pages/profile/create/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h3 class="header">Create a new Profile</h3>
+		<UiRadio :radioTypes="radioTypes" v-model="selectedRadio" />
+		<transition name="fade" mode="out-in">
+			<ProfilePractice
+				v-if="selectedRadio === 'practice'"
+				@toRep="handleRadioChange('salesRep')"
+			/>
+			<ProfileRep v-else-if="selectedRadio === 'salesRep'" />
+		</transition>
+	</div>
+</template>
+
+<script setup lang="ts">
+const radioTypes = [
+	{
+		id: "practice",
+		label: "Practice",
+	},
+	{
+		id: "salesRep",
+		label: "Sales Rep",
+	},
+	{
+		id: "admin",
+		label: "Admin",
+	},
+];
+const selectedRadio = ref(radioTypes[0].id);
+const handleRadioChange = (radioType: string) => {
+	selectedRadio.value = radioType;
+};
+</script>
+
+<style scoped></style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,36 +1,9 @@
 <template>
 	<div>
-		<h3 class="header">Create a new Profile</h3>
-		<UiRadio :radioTypes="radioTypes" v-model="selectedRadio" />
-		<transition name="fade" mode="out-in">
-			<ProfilePractice
-				v-if="selectedRadio === 'practice'"
-				@toRep="handleRadioChange('salesRep')"
-			/>
-			<ProfileRep v-else-if="selectedRadio === 'salesRep'" />
-		</transition>
+		<h3 class="header">Profile</h3>
 	</div>
 </template>
 
-<script setup lang="ts">
-const radioTypes = [
-	{
-		id: "practice",
-		label: "Practice",
-	},
-	{
-		id: "salesRep",
-		label: "Sales Rep",
-	},
-	{
-		id: "admin",
-		label: "Admin",
-	},
-];
-const selectedRadio = ref(radioTypes[0].id);
-const handleRadioChange = (radioType: string) => {
-	selectedRadio.value = radioType;
-};
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>

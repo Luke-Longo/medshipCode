a420fa981456ff86bf169e8b9fe39459bd1e9f07
Luke-Longo <lukelongo0421@gmail.com>
2022-08-23 16:39:08 -0400

    clear

--- components/Profile/Practice.vue
+++ components/Profile/Practice.vue
@@ -1,62 +1,72 @@
 <template>
 	<div class="mx-4">
-		<transition name="fade" mode="out-in">
-			<div class="flex flex-col" v-if="!repSelected">
-				<UiSearchInline
-					v-model="repSearchInput"
-					label="Sales Rep"
-					@search="handleSearch"
-				/>
-				<SalesRepList
-					:items="reps"
-					:list="listTitles"
-					:itemName="'Sales Rep'"
-					@selected="handleSelected"
-					@add="addRep"
-				/>
-				<button
-					class="reverse text-center justify-center mx-auto mt-4"
-					@click="handleSelected(null)"
-				>
-					Add Without Rep
-				</button>
-			</div>
-			<div v-else>
-				<p class="my-4 flex gap-4 items-center">
-					Selected Rep: {{ selectedRep !== null ? selectedRep.name : "None" }}
-					<span
-						class="p-2 rounded-md dark:hover:bg-darkBg cursor-pointer border border-darkBg trans hover:bg-darkSecondary"
-						@click="changeRep"
-						>Change Rep</span
-					>
-				</p>
-				<div class="mt-6 mb-6 grid grid-cols-3 sm:gap-2 lg:grid-cols-4">
-					<FormElementGrid
-						class="lg:w-full"
-						v-for="formElement in formElements"
-						:key="formElement.title"
-						:title="formElement.title"
-						v-model="input[formElement.id].val"
-						:is-valid="input[formElement.id].isValid"
-						:error="formElement.error"
-						:placeholder="formElement.title"
-						:required="formElement.required"
-						@reset-validity="resetValidity(formElement.id)"
+		<div v-if="authStore.isAdmin">
+			<transition name="fade" mode="out-in">
+				<div class="flex flex-col" v-if="!repSelected">
+					<UiSearchInline
+						v-model="repSearchInput"
+						label="Sales Rep"
+						@search="handleSearch"
 					/>
-				</div>
-				<div class="flex justify-center">
-					<button class="w-2/3 mx-auto reverse" @click="createProfile">
-						Create Profile
+					<SalesRepList
+						:items="reps"
+						:list="listTitles"
+						:itemName="'Sales Rep'"
+						@selected="handleSelected"
+						@add="addRep"
+					/>
+					<button
+						class="reverse text-center justify-center mx-auto mt-4"
+						@click="handleSelected(null)"
+					>
+						Add Without Rep
 					</button>
 				</div>
+				<div v-else>
+					<p class="my-4 flex gap-4 items-center">
+						Selected Rep: {{ selectedRep !== null ? selectedRep.name : "None" }}
+						<span
+							class="p-2 rounded-md dark:hover:bg-darkBg cursor-pointer border border-darkBg trans hover:bg-darkSecondary"
+							@click="changeRep"
+							>Change Rep</span
+						>
+					</p>
+					<div class="mt-6 mb-6 grid grid-cols-3 sm:gap-2 lg:grid-cols-4">
+						<FormElementGrid
+							class="lg:w-full"
+							v-for="formElement in formElements"
+							:key="formElement.title"
+							:title="formElement.title"
+							v-model="input[formElement.id].val"
+							:is-valid="input[formElement.id].isValid"
+							:error="formElement.error"
+							:placeholder="formElement.title"
+							:required="formElement.required"
+							@reset-validity="resetValidity(formElement.id)"
+						/>
+					</div>
+					<div class="flex justify-center">
+						<button class="w-2/3 mx-auto reverse" @click="createProfile">
+							Create Profile
+						</button>
+					</div>
+				</div>
+			</transition>
+		</div>
+		<div v-else>
+			<div class="flex flex-col">
+				<h3 class="header">Profile</h3>
+				<p class="my-4">You must be an admin to create a profile.</p>
 			</div>
-		</transition>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { PracticeInput, FormElement } from "~/types/types";
+import { PracticeInput } from "~/types/types";
+import { useAuthStore } from "~/stores/auth";
 
+const authStore = useAuthStore();
 const input: PracticeInput = reactive({
 	practiceName: {
 		val: "",
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,9 +1,38 @@
 <template>
 	<div>
 		<h3 class="header">Profile</h3>
+		<UiSearchInline
+			:label="'Profiles'"
+			v-model="searchInput"
+			@search="searchProfiles"
+		/>
+		<UiRadio :radioTypes="radioTypes" v-model="selectedRadio" />
+		<transition name="fade" mode="out-in">
+			<ProfilePractice
+				v-if="selectedRadio === 'practice'"
+				@toRep="handleRadioChange('salesRep')"
+			/>
+			<ProfileRep v-else-if="selectedRadio === 'salesRep'" />
+		</transition>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const searchInput = ref("");
+const radioTypes = [
+	{
+		id: "practice",
+		label: "Practice",
+	},
+	{
+		id: "salesRep",
+		label: "Sales Rep",
+	},
+];
+const selectedRadio = ref(radioTypes[0].id);
+const handleRadioChange = (radioType: string) => {
+	selectedRadio.value = radioType;
+};
+</script>
 
 <style scoped></style>

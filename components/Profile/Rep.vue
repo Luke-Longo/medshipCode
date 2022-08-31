cfbcd889cffdde3e06588fb8bd9d306d827fcb33
Luke-Longo <lukelongo0421@gmail.com>
2022-08-31 09:13:47 -0400

    we grindin

--- components/Profile/Rep.vue
+++ components/Profile/Rep.vue
@@ -1,15 +1,24 @@
 <template>
 	<div class="mx-4">
-		<div class="grid grid-cols-4 sm:grid-cols-3">
-			<p v-for="key in salesRep"></p>
+		<div class="grid grid-cols-4 sm:grid-cols-3" v-if="hasData">
+			<p v-for="key in salesRep">{{ key }} : {{ salesRep }}</p>
+		</div>
+		<div class="flex justify-center items-center mt-36" v-else>
+			<p class="flex text-xl">Please contact an admin to setup your details</p>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { useProfileStore } from "~~/stores/profile";
+import { SalesRep } from "~~/types/types";
 
 const profileStore = useProfileStore();
 
-const salesRep = profileStore.salesRep;
+const hasData = ref(false);
+if (!!profileStore.salesRep) {
+	hasData.value = true;
+}
+
+// const salesRep: SalesRep = ref(profileStore.salesRep);
 </script>
